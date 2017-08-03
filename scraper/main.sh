939b3de3fbc25a8a9bddf09ef9c4ee8010588f90
Afolabi Badmos <abadmos@pivotal.io>
2017-08-03 09:06:22 -0400

    saves api to file

--- scraper/main.sh
+++ scraper/main.sh
@@ -3,6 +3,13 @@ echo "calling git .."
 #curl https://api.github.com/?access_token=
 #curl https://developer.github.com/v3/rate_limit/
 
-curl -H "Accept: application/vnd.github.mercy-preview+json" \
-  https://api.github.com/search/repositories?q=topic:ruby+rails+starter&sort=stars&order=desc&access_token=
+echo "TOKEN:" $1
+
+curl -K -H "Accept: application/vnd.github.mercy-preview+json" "https://api.github.com/search/repositories?q=topic:angular+seed&sort=stars&order=desc&access_token=$1" >> seedapps.txt
+
+#jq -c '.[]' input.json | while read i; do
+#    echo $i
+#done
+
+#echo $REPO
 
